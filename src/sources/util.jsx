eface9303436b377b34921aaa34051e164af4d00
DerpyCrabs <derpycrabs@gmail.com>
2019-04-15 22:16:50 +0700

    Remove buggy multiselect and useless functions

--- src/sources/util.jsx
+++ src/sources/util.jsx
@@ -9,61 +9,16 @@ export const elementToObject = element =>
     return obj
   }, {})
 
-const zip = (a, b) => {
-  return a.map((e, i) => [e, b[i]])
-}
 export const getSource = source => {
-  if (source.includes('-')) {
-    const sources = source.split('-')
-    const parsePage = async (query, page) => {
-      let pages = []
-      for (source of sources) {
-        pages.push({
-          source,
-          page: await getSource(source).parsePage(query, page)
-        })
-      }
-      return pages
-    }
-    const getPageCount = pages => {
-      return Math.max(
-        ...pages.map(({ source, page }) => getSource(source).getPageCount(page))
-      )
-    }
-    const getThumbs = pages => {
-      return pages
-        .map(({ source, page }) => getSource(source).getThumbs(page))
-        .flat()
-    }
-    const getImageUrl = postUrl => {
-      return getSource(
-        sources.find(source => postUrl.includes(source))
-      ).getImageUrl(postUrl)
-    }
-    return {
-      parsePage,
-      getPageCount,
-      getThumbs,
-      getImageUrl
-    }
+  if (source === 'safebooru') {
+    return Safebooru
+  } else if (source === 'yandere') {
+    return Yandere
+  } else if (source === 'konachan') {
+    return Konachan
+  } else if (source === 'rule34') {
+    return Rule34
   } else {
-    if (source === 'safebooru') {
-      return Safebooru
-    } else if (source === 'yandere') {
-      return Yandere
-    } else if (source === 'konachan') {
-      return Konachan
-    } else if (source === 'rule34') {
-      return Rule34
-    } else {
-      return undefined
-    }
+    return undefined
   }
-}
-
-export const contains = (doc, selector, text) => {
-  var elements = doc.querySelectorAll(selector)
-  return Array.prototype.filter.call(elements, function(element) {
-    return RegExp(text).test(element.textContent)
-  })
 }
