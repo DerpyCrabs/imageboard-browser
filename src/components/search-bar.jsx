25a1d3373570e4338c5c56dd3b0160444c6c8072
DerpyCrabs <derpycrabs@gmail.com>
2019-04-16 23:17:43 +0700

    Factor out search bar from gallery page

---
+++ src/components/search-bar.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import TagsInput from './tags-input'
+
+const SearchBar = ({ initialQuery, source, handleSearch }) => {
+  const [search, setSearch] = useState(decodeURIComponent(initialQuery) + ' ')
+  useEffect(() => setSearch(decodeURIComponent(initialQuery) + ' '), [
+    initialQuery
+  ])
+
+  return (
+    <div className="field has-addons">
+      <div className="control is-expanded">
+        <TagsInput
+          tags={search}
+          setTags={e => setSearch(e)}
+          onEnter={e => handleSearch(source, e)}
+        />
+      </div>
+      <div className="control">
+        <div className="select">
+          <select
+            value={source}
+            onChange={e => handleSearch(e.target.value, search)}
+          >
+            <option value="safebooru">Safebooru</option>
+            <option value="konachan">Konachan</option>
+            <option value="yandere">Yandere</option>
+            <option value="rule34">Rule34</option>
+          </select>
+        </div>
+      </div>
+      <div className="control">
+        <button
+          className="button is-primary"
+          onClick={() => handleSearch(source, search)}
+        >
+          Find
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
