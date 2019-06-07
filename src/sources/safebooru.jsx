8ca597926fb595011725e9c92389db6b22c31578
DerpyCrabs <derpycrabs@gmail.com>
2019-06-07 12:01:16 +0700

    Switch safebooru to HTTPS

--- src/sources/safebooru.jsx
+++ src/sources/safebooru.jsx
@@ -6,7 +6,7 @@ const getPageCount = res =>
 export const getPosts = async (query, page) =>
   fetch(
     `${process.env.REACT_APP_CORS_PROXY ||
-      ''}http://safebooru.org/index.php?page=dapi&s=post&q=index&pid=${page -
+      ''}https://safebooru.org/index.php?page=dapi&s=post&q=index&pid=${page -
       1}&limit=24&tags=${query}`
   )
     .then(res => res.text())
@@ -20,14 +20,14 @@ export const getPosts = async (query, page) =>
 
 const getPost = post => {
   const obj = elementToObject(post)
-  const postUrl = `http://safebooru.org/index.php?page=post&s=view&id=${obj.id}`
+  const postUrl = `https://safebooru.org/index.php?page=post&s=view&id=${obj.id}`
   return {
-    thumbUrl: `${process.env.REACT_APP_CORS_PROXY || ''}http:${
+    thumbUrl: `${process.env.REACT_APP_CORS_PROXY || ''}${
       obj.preview_url
     }`,
     tags: obj.tags,
     postUrl,
-    imageUrl: `${process.env.REACT_APP_CORS_PROXY || ''}http:${obj.file_url}`,
+    imageUrl: `${process.env.REACT_APP_CORS_PROXY || ''}${obj.file_url}`,
     source: 'safebooru',
     sourceTitle: 'safebooru.org'
   }
